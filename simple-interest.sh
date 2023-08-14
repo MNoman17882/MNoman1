ca46b8ae1fafcb28d2aadca55544c64a72fb36da
MNoman17882 <140825956+MNoman17882@users.noreply.github.com>
2023-08-14 17:39:57 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <Muhammad Noman Saqib>
    # Input:
    # p, principal amount
    # t, time period in years
